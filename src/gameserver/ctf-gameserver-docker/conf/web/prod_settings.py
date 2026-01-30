44a688c1d77e180d1920c85226cb331542564468
Nguyen Hieu <117745867+Hieu1Tuoi@users.noreply.github.com>
2026-01-30 15:39:04 +0700

    remove password for application

--- src/gameserver/ctf-gameserver-docker/conf/web/prod_settings.py
+++ src/gameserver/ctf-gameserver-docker/conf/web/prod_settings.py
@@ -61,8 +61,8 @@ CACHES = {
 # ---------------------------------------------------------------------
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'n21dcat021@student.ptithcm.edu.vn'
-EMAIL_HOST_PASSWORD = 'fmyoplzreglgeoxe'
+EMAIL_HOST_USER = '<email>'
+EMAIL_HOST_PASSWORD = '<password>'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = 'PTITHCM A&D CTF'
